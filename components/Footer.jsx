b26aa5799966ff6ca5ed5623d78adadb8e8fae3b
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-03-20 09:14:16 +0600

    next agency

---
+++ components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.carL}>
+        <h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
+        <h1 className={styles.linkTitle}>
+          <a href="/contact" passHref>
+            <span>WORK WITH US</span>
+            <Image src="/img/link.png" width="40" height="40" alt="" />
+          </a>
+        </h1>
+      </div>
+      <div className={styles.carS}>
+        <div className={styles.cardItem}>
+          12 ADAM STREET NY <br /> USA
+        </div>
+        <div className={styles.cardItem}>
+          italimbd@gmail.com <br /> +8801860060068
+        </div>
+      </div>
+      <div className={styles.carS}>
+        <div className={styles.cardItem}>
+          FOLLOW US:
+          <br /> __FB __IN __BE __TW
+        </div>
+        <div className={styles.cardItem}>
+          &#9400;2022 ALIM INTERACTIVE, <br />
+          ALL RIGHTS RESERVED
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/Intro.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../styles/Intro.module.css';
+import Image from 'next/image';
+import Circle from './Circle';
+
+function Intro() {
+  return (
+    <div className={styles.container}>
+      <Circle backgroundColor="#b0ff49" top="-50vh" left="-50vh" />
+      <Circle backgroundColor="#01c686" right="-40vh" />
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          {' '}
+          <span className={styles.brand}>AV0C0D0</span> DIGITAL PRODUCT AGENCY
+        </h1>
+        <p className={styles.desc}>
+          Create live segment and target the right people for message based on
+          their behaviors
+        </p>
+        <button className={styles.button}>Discover</button>
+      </div>
+      <div className={styles.card}>
+        <Image
+          src="/img/Avocado.png"
+          alt="img"
+          width="100%"
+          height="100%"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
