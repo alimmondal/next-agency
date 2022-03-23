d76cf2b34693c7a23dd28946e8ae34b453a8f9ad
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-03-23 06:23:01 +0600

    updated

--- pages/products/[name].js
+++ pages/products/[name].js
@@ -10,14 +10,7 @@ function Product({ product }) {
       <div className={styles.cardL}>
         {product.images.map((img) => (
           <div key={img.id} className={styles.imgContainer}>
-            <Image
-              src={img.url}
-              width="100%"
-              height="100%"
-              objectFit="cover"
-              layout="responsive"
-              alt=""
-            />
+            <Image src={img.url} objectFit="cover" layout="fill" alt="" />
           </div>
         ))}
       </div>
